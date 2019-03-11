2d6e7a5f98660c6f6a407998adf442d2e291a6b1
Niall Fraser <niallfraser92@gmail.com>
2019-03-11 14:58:05 -0500

    users can now add sessions

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,6 +3,8 @@ import { Route } from "react-router-dom"
 import SessionManager from "../modules/SessionManager"
 import UserList from "./users/UserList";
 import UserManager from "../modules/UserManager"
+import SessionList from "./sessions/SessionList";
+import AddSessionForm from "./sessions/SessionAddForm";
 
 class ApplicationViews extends Component {
   state = {
@@ -10,8 +12,14 @@ class ApplicationViews extends Component {
     sessions: []
   }
 
+  addSession = newSession => {
+    return SessionManager.post(newSession)
+    .then(() => SessionManager.sortSessions())
+    .then(sessions => this.setState({sessions: sessions}))
+  }
+
   componentDidMount() {
-    // SessionManager.getAll().then(sessions => this.setState({sessions: sessions}))
+    SessionManager.getAll().then(sessions => this.setState({sessions: sessions}))
 
     UserManager.getAll().then(users => this.setState({users: users}))
 
@@ -27,6 +35,19 @@ class ApplicationViews extends Component {
           {...props}  />
           );
         }} />
+
+      <Route exact path="/sessions" render={props => {
+        return (
+          <SessionList sessions={this.state.sessions}
+                       {...props}  />
+          );
+        }} />
+
+<Route path="/sessions/new" render={(props) => {
+                    return <AddSessionForm {...props}
+                                addSession={this.addSession}
+                                sessions={this.state.sessions} />
+                }} />
     </React.Fragment>
   }
 }
---
+++ src/components/sessions/SessionAddForm.js
@@ -0,0 +1,143 @@
+import React, { Component } from "react";
+
+export default class extends Component {
+
+    state = {
+        date: "",
+        location: "",
+        shots: "",
+        area: "",
+        from: "",
+        scored: "",
+        taken: "",
+        comments:""
+    }
+
+    handleFieldChange = evt => {
+        const stateToChange = {};
+        stateToChange[evt.target.id] = evt.target.value;
+        this.setState(stateToChange);
+      };
+
+      constructNewSession = evt => {
+          evt.preventDefault()
+          const session = {
+              date: this.state.date,
+              location: this.state.location,
+              shotsTaken: this.state.shots,
+              userId: parseInt(sessionStorage.getItem("credentials")),
+              areaOfField: this.state.area,
+              distance: this.state.from,
+              scored: this.state.scored,
+              foot: this.state.taken,
+              comments: this.state.comments
+          }
+
+          this.props.addSession(session)
+          .then(() => this.props.history.push("/sessions"))
+      }
+
+      render() {
+        return (
+            <React.Fragment>
+                <form className="sessionForm">
+                    <div className="form-group">
+                        <label htmlFor="date">Date</label>
+                        <input
+                            type="date"
+                            required
+                            className="form-control"
+                            onChange={this.handleFieldChange}
+                            id="date"
+                            placeholder="Enter Date"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="location">Date</label>
+                        <input
+                            type="text"
+                            required
+                            className="form-control"
+                            onChange={this.handleFieldChange}
+                            id="location"
+                            placeholder="Enter Field"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="shotsTaken">Shots Taken</label>
+                        <input
+                            type="text"
+                            required
+                            className="form-control"
+                            onChange={this.handleFieldChange}
+                            id="shots"
+                            placeholder="Enter Shots Attempted"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="area">Area</label>
+                        <input
+                            type="text"
+                            required
+                            className="form-control"
+                            onChange={this.handleFieldChange}
+                            id="area"
+                            placeholder="eg. central, left, right"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="from">From</label>
+                        <input
+                            type="text"
+                            required
+                            className="form-control"
+                            onChange={this.handleFieldChange}
+                            id="from"
+                            placeholder="How Far out were you?"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="scored">Scored</label>
+                        <input
+                            type="text"
+                            required
+                            className="form-control"
+                            onChange={this.handleFieldChange}
+                            id="scored"
+                            placeholder="How Many Did You Score?"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="taken">Taken With</label>
+                        <input
+                            type="text"
+                            required
+                            className="form-control"
+                            onChange={this.handleFieldChange}
+                            id="taken"
+                            placeholder="Which foot did you shoot with?"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="comments">Comments</label>
+                        <input
+                            type="text"
+                            required
+                            className="form-control"
+                            onChange={this.handleFieldChange}
+                            id="scored"
+                            placeholder="Comments"
+                        />
+                    </div>
+                    <button
+          type="submit"
+          onClick={this.constructNewSession}
+          className="btn btn-primary"
+        >
+          Submit
+        </button>
+                    </form>
+            </React.Fragment>
+        )
+    }
+}
