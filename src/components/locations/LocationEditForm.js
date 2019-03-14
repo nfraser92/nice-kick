f54d5a3e241630d762ac0f3b53d0a3f0088e47f0
Niall Fraser <niallfraser92@gmail.com>
2019-03-14 14:12:38 -0500

    can now edit locations

---
+++ src/components/locations/LocationEditForm.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react"
+import LocationManager from "../../modules/LocationManager";
+
+
+
+export default class LocationEditForm extends Component {
+    state = {
+        name: "",
+        address: "",
+        comments: "",
+    }
+
+    handleFieldChange = evt => {
+        const stateToChange = {}
+        stateToChange[evt.target.id] = evt.target.value
+        this.setState(stateToChange)
+    }
+
+    updateExistingLocation = evt => {
+        evt.preventDefault()
+        const editedLocation = {
+            id: this.props.match.params.locationId,
+            name: this.state.name,
+            address: this.state.address,
+            comments: this.state.comments
+        };
+
+        this.props.put(editedLocation)
+        .then(() => this.props.history.push("/locations"))
+
+    }
+            componentDidMount() {
+                LocationManager.get(this.props.match.params.locationId)
+                .then(location => {
+                    this.setState(location)
+                })
+            }
+
+    render () {
+        return (
+            <React.Fragment>
+                <form className="locationForm">
+                    <div className="form-group">
+                        <label htmlFor="name">Field</label>
+                        <input
+                            type="text"
+                            required
+                            className="form-control"
+                            onChange={this.handleFieldChange}
+                            id="name"
+                            value={this.state.name}
+                        />
+                    </div>
+                    <div className="form-group">
+                          <label htmlFor="address">Address</label>
+                          <input
+                              type="text"
+                              required
+                              className="form-control"
+                              onChange={this.handleFieldChange}
+                              id="address"
+                              placeholder="locaion"
+                              value={this.state.address}
+                          />
+                      </div>
+                      <div className="form-group">
+                          <label htmlFor="comments">Comments</label>
+                          <input
+                              type="text"
+                              required
+                              className="form-control"
+                              onChange={this.handleFieldChange}
+                              id="comments"
+                              placeholder="Comments"
+                              value={this.state.comments}
+                          />
+                      </div>
+                      <button
+            type="submit"
+            onClick={this.updateExistingLocation}
+            className="btn btn-primary"
+          >
+            Submit
+          </button>
+                        </form>
+            </React.Fragment>
+        )
+    }
+}
